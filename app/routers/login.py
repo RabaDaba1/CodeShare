9fdee4a7e9d5443a6e6193c3e74ea28993c44783
Kacper Rabczewski <krabczewski@gmail.com>
2023-10-22 19:19:37 +0200

    fix tamplate path in login.py

--- app/routers/login.py
+++ app/routers/login.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="../templates")
 
 @router.get("/login", response_class=HTMLResponse, tags=["Login"])
 async def login_form(request: Request):
