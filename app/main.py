a800a56dfd6406e7135eaf28d939bb02fff23619
Kacper Rabczewski <krabczewski@gmail.com>
2023-10-22 19:06:36 +0200

    add login route

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+# Import the routes
+from routers import login
+
 app = FastAPI()
+
+app.include_router(login.router)
+
 templates = Jinja2Templates(directory="../templates")
 
 app.mount("/static", StaticFiles(directory="../static"), name="static")
