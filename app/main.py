b44bfae70c55851589d7a6a134ae7f808ce8500b
Kacper Rabczewski <krabczewski@gmail.com>
2023-10-22 19:24:17 +0200

    add register route

--- app/main.py
+++ app/main.py
@@ -3,16 +3,20 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 # Import the routes
-from routers import login
+from routers import login, register
 
+# Create an app object
 app = FastAPI()
 
+# Include the routes
 app.include_router(login.router)
+app.include_router(register.router)
 
+# Mount the templates and static folders
 templates = Jinja2Templates(directory="../templates")
-
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 
+# Route for the home page
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
