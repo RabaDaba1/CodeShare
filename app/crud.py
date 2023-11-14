3fcadf630221bc94dda60b5a7308d729badf28a1
Aleksander <trzcinski@student.agh.edu.pl>
2023-11-14 23:17:40 +0100

    add data functions skeletons

---
+++ app/crud.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+import datetime
+
+def create_user(login: str, username: str, password: str) -> User:
+    pass
+
+def send_friend_request(requester_id: int, receiver_id: int) -> FriendRequest:
+    pass
+
+def like_post(user_id: int, post_id: int) -> PostLike:
+    pass
+
+def create_post(author_id: int, description: str, date: datetime, lang: enum, code: str, output: str) -> Post:
+    pass
+
+def create_comment(author_id: int, post_id: int, content: str, date: datetime) -> Comment:
+    pass
