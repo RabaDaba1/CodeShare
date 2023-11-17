dcbd8d32af0a2e0c69e256eb5ebf4749ce0648dc
Kacper Rabczewski <krabczewski@gmail.com>
2023-11-17 20:53:18 +0100

    create a /user/:username GET endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="../templates")
 
-@router.get("/user/{username}")
-async def get_user(username: str):
-    pass
+@router.get("/user/{username}", response_class=HTMLResponse, tags=["User"])
+async def get_user(request: Request, username: str):
+    return templates.TemplateResponse("user.html", {"request": request, "username": username})
