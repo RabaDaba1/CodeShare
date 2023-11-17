d4f640b2febcd5501eb1851591b3515e7e239142
Kacper Rabczewski <krabczewski@gmail.com>
2023-11-17 21:03:49 +0100

    change /user/:username to /user/:login

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,6 +5,8 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="../templates")
 
-@router.get("/user/{username}", response_class=HTMLResponse, tags=["User"])
-async def get_user(request: Request, username: str):
-    return templates.TemplateResponse("user.html", {"request": request, "username": username})
+@router.get("/user/{login}", response_class=HTMLResponse, tags=["User"])
+async def get_user(request: Request, login: str):
+    # TODO
+    # Get user data from the database
+    return templates.TemplateResponse("user.html", {"request": request, "login": login})
