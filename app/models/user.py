de999ebbeac2a7cbb7f0c43c9817a0263fc58fca
Aleksander <trzcinski@student.agh.edu.pl>
2023-11-21 19:27:20 +0100

    delate methods from user

--- app/models/user.py
+++ app/models/user.py
@@ -7,39 +7,12 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    login = Column(String(16))
+    login = Column(String(16), unique=True)
     username = Column(String(32))
     password = Column(String(32))
     bio = Column(String(300), nullable=True)
     pictureUrl = Column(String(200), nullable=True)
-    
-    def compare_passwords(str1, str2):
-        errors = []
-        if len(str1) > 32 or len(str1 < 6):
-            errors.append("The password should be between 6 and 32 characters long")
 
-        if not (any(char.isdigit() for char in str1) and any(char.isalpha() for char in str1)):
-            errors.append("Password should contain both letters and numbers.")
-
-        if str1 != str2:
-            errors.append("The given passwords do not match.")
-
-        if errors:
-            raise HTTPException("\n".join(errors))
-        
-    def __init__(self, log, un, pas, rep_pass):
-        try:
-           if len(un) > 32 or len(un < 6):
-            raise HTTPException("The username should be between 6 and 32 characters long")
-           self.compare_passwords(pas, rep_pass) 
-           self.login = log
-           self.username = un
-           self.password = pas
-           self.bio = Null
-           self.pictureUrl = Null
-         
-        except HTTPException as e:
-            print(e)
             
         
     
