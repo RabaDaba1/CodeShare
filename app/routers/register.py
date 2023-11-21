66f66491848d95eca3598842ed3f3ec091f2aea9
SergZeliuk <serhiizeliuk@student.agh.edu.pl>
2023-11-21 20:03:27 +0100

    Revert "register endpoint added"
    This reverts commit 78808207e54126855b5e22f7f376f38677009e26.

--- app/routers/register.py
+++ app/routers/register.py
@@ -1,11 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
-from app.database import SessionLocal
-from crud import create_user
-from models.user import User
-from database import get_db
 
 router = APIRouter()
 
@@ -15,10 +10,5 @@ templates = Jinja2Templates(directory="../templates")
 async def register_form(request: Request ):
     return templates.TemplateResponse("register.html", {"request": request})
 
-@router.post("/users/")
-async def create_user(login: str, username: str, password: str, repeat_password: str, db: Session = Depends(get_db)):
-    if password != repeat_password:
-        raise HTTPException(status_code=400, detail="Passwords do not match")
-    return create_user(db=db, login=login, username=username, password=password, repeat_password=repeat_password)
 
 # TODO: Create a POST endpoint for the registration form
