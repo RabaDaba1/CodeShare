09a3df0d6bc4f91491896a9720722c6ee1422d8c
Kacper Rabczewski <krabczewski@gmail.com>
2023-12-27 15:59:43 +0100

    create redirect from /feed to / for loged out users

--- app/routers/feed.py
+++ app/routers/feed.py
@@ -3,8 +3,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
-from models.user import User
-from crud import get_user_by_login, create_post, get_current_user
+from crud import create_post, get_current_user, get_all_posts
 from database import get_db
 
 router = APIRouter()
@@ -13,16 +12,23 @@ templates = Jinja2Templates(directory="../templates")
 
 @router.get("/feed", response_class=HTMLResponse, tags=["Feed"])
 async def feed(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("feed.html", {"request": request})
+    # TODO: Get all post from the database    
+    posts= []
+    
+    # If user is not logged in, redirect to login page
+    token = request.cookies.get("access_token")
+    if not token:
+        return RedirectResponse(url="/login", status_code=303)
+
+    return templates.TemplateResponse("feed.html", {"request": request, "posts": posts})
 
 @router.get("/feed/{post_id}", response_class=HTMLResponse, tags=["Feed"])
-async def post_detailed(request: Request, post_id: int):
-    # TODO: Get the post from the database and pass it to the template
+async def post_detailed(request: Request, post_id: int, db: Session = Depends(get_db)):    
     return templates.TemplateResponse("post_detailed.html", {"request": request})
 
 @router.post("/feed", response_model_exclude_unset=True)
-async def create_post(request: Request, description: str = Form(...), programming_language: str = Form(...), code: str = Form(...), output: str = Form(...), db: Session = Depends(get_db)):
-    #  Get the access token from the cookie
+async def new_post(request: Request, description: str = Form(...), programming_language: str = Form(...), code: str = Form(...), output: str = Form(...), db: Session = Depends(get_db)):
+    # Get the access token from the cookie
     token = request.cookies.get("access_token")
     
     if not token:
@@ -31,10 +37,8 @@ async def create_post(request: Request, description: str = Form(...), programmin
     # Get the current user from the database
     current_user = get_current_user(db, token)
     
-    print(current_user.id, current_user.login)
-    
     # Create the post
-    create_post(db, current_user.id, description, programming_language, code, output)
+    create_post(db, current_user.userId, description, programming_language, code, output)
     
     # Redirect the user to the feed page
     return RedirectResponse(url="/feed", status_code=303)
