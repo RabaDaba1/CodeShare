539773e6c51c7245739e5eff59f740326a5c4354
Kacper Rabczewski <krabczewski@gmail.com>
2024-01-11 18:58:02 +0100

    add error handling to /user/login

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from crud import get_user_posts, get_current_user
+from crud import get_user_posts, get_current_user, get_user_by_login
 from database import get_db
 from sqlalchemy.orm import Session
 from models.user import User
@@ -11,8 +11,10 @@ templates = Jinja2Templates(directory="../templates")
 
 @router.get("/user/{login}", response_class=HTMLResponse, tags=["User"])
 async def user_page(request: Request, login: str, db: Session = Depends(get_db)):
-    # TODO: Get user data from the database and pass it to the template
-
+    user = get_user_by_login(db, login)
+    if user is None:
+        return templates.TemplateResponse("error.html", {"request": request, "message": f"User {login} not found", "detailed_message": "Sorry, we couldn't find the user you were looking for."})
+    
     posts = get_user_posts(db, login)
     
     current_user = get_current_user(db, request.cookies.get("access_token"))
@@ -22,6 +24,8 @@ async def user_page(request: Request, login: str, db: Session = Depends(get_db))
 
 @router.get("/user/{login}/settings", response_class=HTMLResponse, tags=["User"])
 async def user_settings(request: Request, login: str):
+    
+    
     return templates.TemplateResponse("user_settings.html", {"request": request, "login": login})
 
 # TODO: Create a PUT endpoint for updating user information at /user/{username}
